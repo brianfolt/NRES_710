66d7966094212d610301dab96e77446b98efd79c
Brian Folt <brianfolt@Brians-MacBook-Pro-2.local>
2024-08-27 08:17:48 -0700

    Cleaning the repo

--- LECTURE1.R
+++
@@ -1,5 +0,0 @@
-
-#  NRES 710, Lecture 1                         
-#    University of Nevada, Reno     
-#    Basic Concepts of Probability and Statistics      
-
--- LECTURE3.R
+++
@@ -1,6 +0,0 @@
-
-#  NRES 710, Lecture 3 ---------------------------
-#  University of Nevada, Reno   
-
-#  Taxonomy of statistics                            
-
