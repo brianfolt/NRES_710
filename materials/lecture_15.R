082c390776b7ce7d15631ae6697718912cb4a15b
Brian Folt <brianfolt@Brians-MacBook-Pro-2.local>
2024-09-05 17:13:41 -0700

    populating full lecture 15

--- materials/lecture_15.R
+++ materials/lecture_15.R
@@ -1,5 +1,29 @@
 
-#  NRES 710, Multi-Variable Modeling - Interactions (continued)
+#  NRES 710, Random Effect Models
 #     University of Nevada, Reno
-#     Paper discussion of interactions
+#     Multi-variable models with random effects
+
+################### 'Truth' #################### 
+### Lecture 15: code to simulate data for class
+
+# Set the seed for reproducibility
+set.seed(123)
+
+# Simulate data
+Year <- rep(1:10, each = 10)
+
+# Between-year error
+MeanYear <- rep(rnorm(10, 0, 10), each = 10)
+
+# Within-year error
+Error <- rnorm(length(Year), 0, 1)
+
+# Response variable
+Abundance <- 50 + MeanYear + Error
+
+# Save the data
+datum <- data.frame(Year = Year, MeanYear = MeanYear, Error = Error, Abundance = Abundance)
+
+# Save the data
+write.csv(datum, "lecture_15_dataset1.csv", row.names = FALSE)
 
