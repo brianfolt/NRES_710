eddb7279cf2d18f98b8a396a0e579125de08d7d8
Brian Folt <brianfolt@Brians-MacBook-Pro-2.local>
2024-09-16 08:44:10 -0700

    prepping lecture 6 for class

--- materials/lecture_6.R
+++ materials/lecture_6.R
@@ -16,14 +16,14 @@ abline(results)
 # Examine the results 
 summary(results)
 
-# Make a prediction for when rainfall = 5 cm
-4.08 + 2.82 * 5
+# Make a prediction for when rainfall = 3 cm
+1.69 + 3.34 * 3
 
 # Examine the help file
 help(predict.lm)
 
 # Create predict data
-datumPredict <- data.frame(rainfall = 5)
+datumPredict <- data.frame(rainfall = 3)
 datumPredict
 
 # Create a new object called predictions
@@ -32,12 +32,19 @@ predictions
 
 predictions[1,"upr"] - predictions[1,"fit"]
 
-# Examine summary
+# Let's look at information about our data
 summary(datum)
-
+# Minimum rainfall was closer to 0, maximum was closer to 10. 
 # Let's make predictions between 0 -- 10 cm of rainfall
-# And let's try to make 20 predictions across this interval: 10cm/20predictions = a prediction every 0.5 cm
-datumPredict <- data.frame(rainfall = seq(from = 0, to = 10, by = 0.5))
+
+# And let's try to make 20 predictions across this interval:
+# 10 cm / 20 predictions = a prediction every 0.5 cm
+xSequence <- seq(from = 0, to = 10, by = 0.5)
+xSequence
+
+# Turn this into a dataframe with a column called 'rainfall'
+datumPredict <- data.frame(rainfall = xSequence)
+datumPredict
 
 # Make the predictions
 predictions <- predict(results, datumPredict, interval = "prediction")
@@ -48,11 +55,11 @@ head(predictions)
 # Make it a dataframe to make it easier to add data to it.
 predictions <- as.data.frame(predictions)
 
-# Add a new column
-predictions$rainfall <- seq(from = 0, to = 10, by = 0.5)
+# Add a new column called 'rainfall'
+predictions$rainfall <- xSequence
 
 # Examine
-predictions
+head(predictions)
 
 # Very simple way to plot the prediction intervals
 matplot(predictions$rainfall, predictions[,1:3], type = "l")
@@ -66,7 +73,7 @@ plot(biomass ~ rainfall, data = datum, xlim = c(0, 10), ylim = c(0, 40), xlab = 
 par(new = TRUE)
 plot(fit ~ rainfall, data = predictions, type = "l", xlim = c(0, 10), ylim = c(0, 40), ylab = "", xlab = "")
 par(new = TRUE)
-plot(lwr ~ rainfall, data = predictions, type = "l", xlim = c(0, 10), ylim = c(0, 40), ylab = "", xlab = "")
+plot(lwr ~ rainfall, data = predictions, type = "l", xlim = c(0, 10), ylim = c(0, 40), ylab = "", xlab = "", lty=2)
 par(new = TRUE)
-plot(upr ~ rainfall, data = predictions, type = "l", xlim = c(0, 10), ylim = c(0, 40), ylab = "", xlab = "")
+plot(upr ~ rainfall, data = predictions, type = "l", xlim = c(0, 10), ylim = c(0, 40), ylab = "", xlab = "", lty=2)
 
